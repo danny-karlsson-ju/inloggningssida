f2ea6b57c2e9a0f82e114268f9faa90d474694ff
danny-karlsson-ju <kada25lb@student.ju.se>
2025-09-08 16:00:41 +0200

    wrong login and small adjustments to login html

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ let namnInput = document.getElementById("namn")
 let lösenordInput = document.getElementById("lösenord")
 let loginButton = document.getElementById("loginButton")
 
+let failLogin = document.createElement("p")
+
 loginButton.addEventListener("click", function() {
     if (namnInput.value === namn && lösenordInput.value === lösenord) {
         window.location.href = "welcome.html";
+    } else {
+        failLogin.innerHTML = "Your username or password may be incorrect."
+        document.getElementById("failMessageDiv").appendChild(failLogin);
     }
     
     console.log(namnInput.value);
