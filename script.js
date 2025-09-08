1f68f02f27415137abc5915b6ab0199b9282974d
danny-karlsson-ju <kada25lb@student.ju.se>
2025-09-08 15:28:26 +0200

    correct login change window location

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const namn = "test"
 const lösenord = "1234"
+
+let namnInput = document.getElementById("namn")
+let lösenordInput = document.getElementById("lösenord")
+let loginButton = document.getElementById("loginButton")
+
+loginButton.addEventListener("click", function() {
+    if (namnInput.value === namn && lösenordInput.value === lösenord) {
+        window.location.href = "welcome.html";
+    }
+    
+    console.log(namnInput.value);
+    console.log(lösenordInput.value);
+});
